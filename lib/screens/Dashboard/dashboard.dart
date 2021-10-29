3f83e039260f230182ae3074cc52e41d2e654a72
AdityaChaudhary18 <sanjords@gmail.com>
2021-10-30 01:34:35 +0530

    init provider and frontend for login

---
+++ lib/screens/Dashboard/dashboard.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+
+import '../../authentication_service.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text("Sign Out"),
+            onPressed: () {
+              context.read<AuthenticationService>().signOut();
+            },
+          ),
+          Center(
+            child: Text("HI BIYCHES"),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:sizer/sizer.dart';
+import 'package:chef_gram/utils/RoundedButton.dart';
+
+import '../../authentication_service.dart';
+import '../../constants.dart';
+
+class LogInPage extends StatefulWidget {
+  const LogInPage({Key? key}) : super(key: key);
+
+  @override
+  _LogInPageState createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+            child: Column(
+              children: [
+                Text(
+                  "Spice",
+                  style: TextStyle(fontSize: 30.sp),
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                TextFormField(
+                  decoration: authTextFieldDecoration,
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  textAlign: TextAlign.center,
+                  decoration: authTextFieldDecoration.copyWith(
+                    labelText: "Password",
+                    hintText: "Enter your Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 2.h,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: RoundedButton(
+                    color: Color(0xFF004AAD),
+                    onPressed: () {
+                      context.read<AuthenticationService>().signIn(
+                          name: emailController.text.trim(),
+                          password: passwordController.text.trim());
+                    },
+                    text: "LOG IN",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
