b21e317bf7f62fec58220c1ba732b47a3b31ef13
AdityaChaudhary18 <sanjords@gmail.com>
2021-11-01 01:19:15 +0530

    Dashboard refined with slidable

--- lib/screens/Dashboard/dashboard.dart
+++ lib/screens/Dashboard/dashboard.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/src/provider.dart';
 import 'package:sizer/sizer.dart';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../../authentication_service.dart';
 import '../../database_service.dart';
 
@@ -17,12 +17,11 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-
   Future<List> getShops() async {
     List<Map<String, dynamic>> shopDetails = [];
     shopDetails.clear();
     for (var shop
-        in Provider.of<DatabaseService>(context).shopsToVisit) {
+        in Provider.of<DatabaseService>(context, listen: true).shopsToVisit) {
       await FirebaseFirestore.instance.doc(shop["shopRef"].path).get().then(
             (value) => shopDetails.add({
               "shopName": value["shopName"],
@@ -43,14 +42,8 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         elevation: 10,
-        leading: ElevatedButton(
-          style: ButtonStyle(elevation: MaterialStateProperty.all(0.0)),
-          child: Icon(Icons.refresh),
-          onPressed: () {
-            getShops();
-          },
-        ),
         title: Text("Dashboard"),
         actions: [
           ElevatedButton(
@@ -65,60 +58,75 @@ class _DashboardState extends State<Dashboard> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            child: FutureBuilder(
-              future: getShops(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (!snapshot.hasData) {
+      body: Container(
+        child: FutureBuilder(
+          future: getShops(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
                   return Column(
                     children: [
-                      Text('No DATA'),
-                    ],
-                  );
-                } else {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: EdgeInsets.symmetric(vertical: 1.h, horizontal: 2.w),
+                      Slidable(
+                        actionPane: SlidableDrawerActionPane(),
+                        actionExtentRatio: 0.25,
                         child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 1,
+                          color: Colors.white,
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: snapshot.data[index]['isVisited']
+                                  ? Colors.indigoAccent
+                                  : Colors.red,
+                              child: Icon(snapshot.data[index]['isVisited']
+                                  ? Icons.check
+                                  : Icons.clear),
+                              foregroundColor: Colors.white,
                             ),
-                            borderRadius: BorderRadius.circular(3.w),
-                          ),
-                          child: Padding(
-                            padding:
-                            EdgeInsets.symmetric(horizontal: 1.w, vertical: 1.h),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  snapshot.data[index]['shopName'],
-                                  style: TextStyle(fontSize: 18.sp),
-                                ),
-                                Text(snapshot.data[index]['shopOwner']),
-                                Text(snapshot.data[index]['isVisited'].toString()),
-                              ],
+                            title: Text(
+                              snapshot.data[index]['shopName'],
+                              style: TextStyle(fontSize: 18.sp),
+                            ),
+                            subtitle: Text(
+                              snapshot.data[index]['shopOwner'],
+                              style: TextStyle(fontSize: 12.sp),
                             ),
                           ),
                         ),
-                      );
-                    },
+                        secondaryActions: [
+                          IconSlideAction(
+                            caption: 'Take Order',
+                            color: Colors.blue,
+                            icon: Icons.add,
+                            onTap: () {},
+                          ),
+                          IconSlideAction(
+                            caption: 'Get Details',
+                            color: Colors.deepPurple,
+                            icon: Icons.info_outline,
+                            onTap: () {},
+                          ),
+                          IconSlideAction(
+                            caption: 'Mark Entry',
+                            color: Colors.green,
+                            icon: Icons.check,
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ],
                   );
-                }
-              },
-            ),
-          ),
-        ],
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
 }
-
-
